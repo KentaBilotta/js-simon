3ddbf3a87d115d375da6f514d4e47c1c93e609cb
Kenta Bilotta <kentabilotta20@gmail.com>
2022-11-04 18:17:26 +0100

    quarto commit: script completato

--- js/script.js
+++ js/script.js
@@ -3,21 +3,8 @@ const btnPlay = document.querySelector("#btn-play");
 let eleNumbers = document.querySelector(".numbers");
 
 btnPlay.addEventListener("click", function() {
-    const timer = setInterval(countdown, 1000);
-let i = 10;
-
-function countdown() {
-    eleTimer.innerHTML = i;
-    i--;
-
-    if (i == -1) {
-        setTimeout(auguri, 1000);
-        function auguri() {
-            eleTimer.innerHTML = "Fine!"
-        }
-        clearInterval(timer);
-    }
-}
+const timer = setInterval(countdown, 1000);
+let counter = 30;
 
 const Numbers = [];
 
@@ -29,11 +16,38 @@ for (let i = 0; i < 5; i++) {
     Numbers.push(randomNumber);
 }
 
-console.log(Numbers);
 eleNumbers.innerHTML = Numbers;
 
 function getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
+function countdown() {
+    eleTimer.innerHTML = counter;
+
+    if (counter === 0) {
+        eleNumbers.innerHTML = "";
+    }
+
+    counter--;
+
+    if (counter == -1) {
+        setTimeout(auguri, 1000);
+        function auguri() {
+            eleTimer.innerHTML = "Fine!"
+            let points = 0;
+            for (let i = 1; i <= 5; i++) {
+                const userInput = prompt("Inserisci il numero "+ i);
+                if (Numbers.includes(parseInt(userInput))) {
+                    points++;
+                    console.log(points);
+                } else {
+                    eleNumbers.innerHTML = "Hai indovinato: " + points + " numeri";
+                }
+            }
+        }
+        clearInterval(timer);
+    }
 }
 
 
